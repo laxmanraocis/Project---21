b3154b12184d9cd373b31dc72454e21ab1db8a6a
laxmanraocis <77572994+laxmanraocis@users.noreply.github.com>
2021-02-06 15:20:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var canvas;
+var music;
+var box;
+var surface1, surface2, surface3, surface4;
+
+function preload(){
+    music = loadSound("music.mp3");
+    
+}
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+    surface1 = createSprite(100,750,200,50);
+    surface1.shapeColor = "red";
+
+    surface2 = createSprite(300,750,200,50);
+    surface2.shapeColor = "green";
+    
+    surface3 = createSprite(500,750,200,50);
+    surface3.shapeColor = "blue";
+
+    surface4 = createSprite(700,750,200,50);
+    surface4.shapeColor = "pink";
+
+  //create box sprite and give velocity
+    box = createSprite(random(20,750));
+    box.velocityY = 3;
+    box.scale = 0.30;
+
+}
+
+function draw() {
+    background(100);
+    //create edgeSprite
+    egdes = createEdgeSprites();
+
+    //add condition to check if box touching surface and make it box
+    if(box.isTouching(surface1) && box.bounceOff(surface1)){
+        box.shapeColor = "red";
+        box.destroyEach();
+
+    }
+
+    if(box.isTouching(surface2) && box.bounceOff(surface2)){
+        box.shapeColor = "green";
+        box.destroyEach();
+
+    }
+
+    if(box.isTouching(surface3) && box.bounceOff(surface3)){
+        box.shapeColor = "blue";
+        box.destroyEach();
+
+    }
+
+    if(box.isTouching(surface4) && box.bounceOff(surface4)){
+        box.shapeColor = "pink";
+        box.destroyEach();
+
+    }
+    drawSprites();
+}
